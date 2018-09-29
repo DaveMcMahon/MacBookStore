b8199cc6dec62d35b3c83f06821910fcfbe55d5c
DaveMcMahon <davidmcmhn@gmail.com>
2018-09-29 11:05:02 -0700

    Initial Commit

---
+++ MacBookStore/App_Start/RouteConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MacBookStore
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Whatever",
+                url: "book/now/what/{age}/{name}",
+                defaults: new { controller = "Book", Action = "DisplayInfo"}
+            );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
