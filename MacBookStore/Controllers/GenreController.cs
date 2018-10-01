fd8870bd559bc4d5e8511e4922864613875cc63d
DaveMcMahon <davidmcmhn@gmail.com>
2018-10-01 11:17:24 -0700

    Added Genre and Create Controller

---
+++ MacBookStore/Controllers/GenreController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MacBookStore.Controllers
+{
+    public class GenreController : Controller
+    {
+        private Models.ApplicationDbContext db;
+
+        public GenreController()
+        {
+            db = new Models.ApplicationDbContext();
+        }
+
+        // GET: Genre
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+        }
+    }
+}
