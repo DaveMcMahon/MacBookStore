e472c121a61301fa0b8a2b89dd72821442e13a02
DaveMcMahon <davidmcmhn@gmail.com>
2018-10-02 11:36:10 -0700

    Added functionality to the Create button within the Genre html

--- MacBookStore/Controllers/GenreController.cs
+++ MacBookStore/Controllers/GenreController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MacBookStore.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -18,6 +19,21 @@ namespace MacBookStore.Controllers
         // GET: Genre
         public ActionResult Index()
         {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Genre genre)
+        {
+            if(ModelState.IsValid)
+            {
+                db.Genres.Add(genre);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
             return View();
         }
 
